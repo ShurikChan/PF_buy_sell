700f902ed43537d81ab06fb09f8d9379f3d05bcd
Shurik <156313194+ShurikChan@users.noreply.github.com>
2024-12-29 21:22:43 +0300

    Update HOW_TO_USE.py

--- HOW_TO_USE.py
+++ HOW_TO_USE.py
@@ -10,3 +10,7 @@ from pump_fun_sell import sell
 
 #sell("mint_address", sell_percentage = 10, close_token_account = False) - if u want sell 10% or any another % like 10-20-30-40-50 and so on
 
+# in utils.py u can find confirm_txn function that checks if tx was successful or not, use it if u need
+
+# also u should know that pf buy/sell funcs send TXs using JITO bundle, so they r faster than usual
+# id u don't need jito, u should modify the code and delete jito logic
